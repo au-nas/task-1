f614c9849f7edfe3a7a5227c1d07318a412df152
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-06-10 18:41:33 +0300

    without JSX commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import ReactLogo from './assets/react.svg'; // декларативный стиль показывает что должно отобразиться
-// import ReactLogo from './assets/react.svg?react';
+import { createElement } from 'react';
+import ReactLogo from './assets/react.svg';
 
-export const App = () => { // императивный стиль
-	return ( // все остальное декларативный
-		<div className="App">
-			<header className="App-header">
-				{/* <ReactLogo /> */}
-				<img src={ReactLogo} className="App-logo" alt="logo" />
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p> {new Date().getFullYear()} </p>  /* императивный стиль */
-			</header>
-		</div>
+export const App = () => {
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement('header', { className: 'App-header' }),
+		createElement('img', {
+			src: ReactLogo,
+			className: 'App-logo',
+			alt: 'logo',
+		}),
+		createElement(
+			'a',
+			{
+				className: 'App-link',
+				href: 'https://reactjs.org',
+				target: '_blank',
+				rel: 'noopener noreferrer',
+			},
+			'Learn React',
+		),
+		createElement('p', null, new Date().getFullYear()),
 	);
 };
